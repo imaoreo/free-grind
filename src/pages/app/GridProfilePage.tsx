fbcd8af78593459b5b59b243f9c91a4e53fbaad6
Jay (Oreo 3) <jaygithub@estopia.net>
2026-05-20 15:26:42 +0100

    Update src/pages/app/GridProfilePage.tsx

--- src/pages/app/GridProfilePage.tsx
+++ src/pages/app/GridProfilePage.tsx
@@ -306,7 +306,7 @@ export function GridProfilePage() {
 		const nextParams = new URLSearchParams();
 		nextParams.set("targetProfileId", targetProfileId);
 		nextParams.set("returnTo", safeReturnTo);
-        appLog.error("Profile", targetProfileId);
+        appLog.info("Profile", targetProfileId);
 		navigate(`/chat?${nextParams.toString()}`);
 	};
 
