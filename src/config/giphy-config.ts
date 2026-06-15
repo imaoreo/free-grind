ef07d79b5fc614ff8e5471a75b5b833eb0355986
Florian <florian.hess@azon.ai>
2026-06-15 11:56:45 +0200

    Move giphy config to separate file

---
+++ src/config/giphy-config.ts
@@ -0,0 +1,5 @@
+export const GIPHY_API_KEY = import.meta.env.VITE_GIPHY_API_KEY as string | undefined ?? "zf7gBZ531pHaTJN3ne0jDdAepsNreB2X";
+
+export const GIPHY_BASE = "https://api.giphy.com/v1/gifs";
+export const GIPHY_RECENT_GIFS_KEY = "fg-recent-gifs";
+export const GIPHY_RECENT_GIFS_MAX = 20;
